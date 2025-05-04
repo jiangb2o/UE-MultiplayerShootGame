0d0bb92dbfad037d358c13b41eeec1085e57ac17
jiangbo <jiangb2o@foxmail.com>
2025-05-04 17:17:14 +0800

    Character Movement

--- Source/MultiplayerShootGame/Character/BlasterCharacter.cpp
+++ Source/MultiplayerShootGame/Character/BlasterCharacter.cpp
@@ -28,15 +28,54 @@ void ABlasterCharacter::BeginPlay()
 	
 }
 
-void ABlasterCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
 void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &ABlasterCharacter::Jump);
+	
+	// 将project setting设置的Axis Mapping绑定对应的方法
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ABlasterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ABlasterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ABlasterCharacter::Turn);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &ABlasterCharacter::LookUp);
+}
+
+void ABlasterCharacter::MoveForward(float Value)
+{
+	if(Controller != nullptr && Value != 0.0f)
+	{
+		// 获得Controller的Yaw旋转值, 转换为旋转矩阵后获取沿X轴的方向向量
+		const FRotator YawRotation(0.0f, Controller->GetControlRotation().Yaw, 0.0f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ABlasterCharacter::MoveRight(float Value)
+{
+	if(Controller != nullptr && Value != 0.0f)
+	{
+		// 获得Controller的Yaw旋转值, 转换为旋转矩阵后获取沿Y轴的方向向量
+		const FRotator YawRotation(0.0f, Controller->GetControlRotation().Yaw, 0.0f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ABlasterCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ABlasterCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+void ABlasterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
 
 }
 
